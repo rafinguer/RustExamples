4e6769f9c8a748be1eed19af3fe044e8883a2ba7
Rafael Hernamperez <rafinguer@gmail.com>
2022-10-09 00:13:19 +0200

    Updated .gitignore. Unnecessary folders and files deleted

--- cargo_hello_world/target/debug/cargo_hello_world.d
+++
@@ -1,1 +0,0 @@
-E:\proyectos\RustExamples\cargo_hello_world\target\debug\cargo_hello_world.exe: E:\proyectos\RustExamples\cargo_hello_world\src\main.rs
--- cargo_hello_world/target/debug/deps/cargo_hello_world.d
+++
@@ -1,5 +0,0 @@
-E:\proyectos\RustExamples\cargo_hello_world\target\debug\deps\cargo_hello_world.exe: src\main.rs
-
-E:\proyectos\RustExamples\cargo_hello_world\target\debug\deps\cargo_hello_world.d: src\main.rs
-
-src\main.rs:
